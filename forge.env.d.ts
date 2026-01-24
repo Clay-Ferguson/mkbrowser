277bbc36319f153fc59084cf10941ae1abf97a88
Clay Ferguson <wclayf@gmail.com>
2026-01-24 02:28:03 -0600

    fixed linter error

--- forge.env.d.ts
+++ forge.env.d.ts
@@ -1,1 +1,5 @@
 /// <reference types="@electron-forge/plugin-vite/forge-vite-env" />
+
+// Electron Forge Vite plugin injects these at build time
+declare const MAIN_WINDOW_VITE_DEV_SERVER_URL: string | undefined;
+declare const MAIN_WINDOW_VITE_NAME: string;
