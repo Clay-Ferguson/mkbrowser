58bad08c14c3fcdba37ce7bc414bbedfcc761263
Clay Ferguson <wclayf@gmail.com>
2026-02-16 01:13:56 -0600

    sort menu text update

--- src/components/menus/SortPopupMenu.tsx
+++ src/components/menus/SortPopupMenu.tsx
@@ -3,11 +3,11 @@ import type { SortOrder } from '../../store/types';
 import PopupMenu, { PopupMenuItem } from './base/PopupMenu';
 
 const sortOptions: { value: SortOrder; label: string }[] = [
-  { value: 'alphabetical', label: 'Alphabetical' },
-  { value: 'created-chron', label: 'Created Time (chron)' },
-  { value: 'created-reverse', label: 'Created Time (reverse-chron)' },
-  { value: 'modified-chron', label: 'Modified Time (chron)' },
-  { value: 'modified-reverse', label: 'Modified Time (reverse-chron)' },
+  { value: 'alphabetical', label: 'Filename' },
+  { value: 'created-chron', label: 'Created Time' },
+  { value: 'created-reverse', label: 'Created Time (rev-chron)' },
+  { value: 'modified-chron', label: 'Modified Time' },
+  { value: 'modified-reverse', label: 'Modified Time (rev-chron)' },
 ];
 
 interface SortPopupMenuProps {
